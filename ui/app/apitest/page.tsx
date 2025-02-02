13d0ddd43ea53c1951dfab28ae5ee7f08131cf9b
suneel-nadipalli <suneel.nadipalli@duke.edu>
2025-02-02 11:25:43 -0500

    Added users page and api functionality

---
+++ ui/app/apitest/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const APITestPage = async () => {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ping`);
+
+  const data = await res.json();
+
+  console.log(data);
+
+  return (
+    <>
+      <div>Welcome to the API Test Page</div>
+      <div>Message From API: {data.status}</div>
+    </>
+  )
+}
+
+export default APITestPage
