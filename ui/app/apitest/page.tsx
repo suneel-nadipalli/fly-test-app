e1c84669ae45aba23fb04953f8b65b2e1c8ba310
suneel-nadipalli <suneel.nadipalli@duke.edu>
2025-03-15 13:40:33 -0400

    Testing vercel update 3

--- ui/app/apitest/page.tsx
+++ ui/app/apitest/page.tsx
@@ -6,7 +6,13 @@ const APITestPage = async () => {
 
   const apiURL = process.env.NEXT_PUBLIC_API_URL;
 
-  const res = await fetch(`${apiURL}/ping`);
+  // const res = await fetch(`${apiURL}/ping`);
+
+  const res = await fetch(`${apiURL}/ping`, {
+    headers: {
+      "Cache-Control": "no-store", // Prevents caching
+    },
+  });
 
   const data = await res.json();
 
